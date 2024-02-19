c51cbdbb00fb394727df6caf7ac2470ef851f762
Wojciech Jedynak <wjedynak@gmail.com>
2024-02-19 17:24:03 +0900

    first migration

---
+++ server/migrations/001_create_tables.sql
@@ -0,0 +1,25 @@
+create table state (
+    from_ bigint not null,
+    to_ bigint not null,
+    step bigint not null
+);
+
+insert into
+    state(from_, to_, step)
+values
+    (1, 1000000000, 1000000);
+
+create table queue (
+    client_name text primary key,
+    task_from bigint not null,
+    task_to bigint not null,
+    queued_at timestamp not null default current_timestamp
+);
+
+create table results (
+    task_from bigint not null,
+    task_to bigint not null,
+    result text not null,
+    client_name text not null,
+    finished_at timestamp default current_timestamp
+);
